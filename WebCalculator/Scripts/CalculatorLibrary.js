0b701af4f50e1dacd2cbc80aeee7c897f04f9c6c
sjunaedy <stephanusj83@gmail.com>
2014-04-15 22:22:46 -0500

    new

---
+++ WebCalculator/Scripts/CalculatorLibrary.js
@@ -0,0 +1,22 @@
+﻿var txtInput;
+ 
+
+function initialize() {
+    txtInput = document.getElementById('txtInput');
+    document.getElementById('btn1').addEventListener('click', numberClick, false);
+    document.getElementById('btn2').addEventListener('click', numberClick, false);
+    document.getElementById('btn3').addEventListener('click', numberClick, false);
+    document.getElementById('btn4').addEventListener('click', numberClick, false);
+    document.getElementById('btn5').addEventListener('click', numberClick, false);
+    document.getElementById('btn6').addEventListener('click', numberClick, false);
+    document.getElementById('btn7').addEventListener('click', numberClick, false);
+    document.getElementById('btn8').addEventListener('click', numberClick, false);
+    document.getElementById('btn9').addEventListener('click', numberClick, false);
+    document.getElementById('btn0').addEventListener('click', numberClick, false);
+}
+
+function numberClick()
+{
+    txtInput.value = txtInput.value == '0' ?
+        this.innerText : txtInput.value + this.innerText;
+}
---
+++ WebCalculator/Scripts/tests.js
@@ -0,0 +1,19 @@
+﻿module('Calculator Test Suite', { setup: function () { initialize();}})
+
+test("Button Click Test", function () {
+    var buttonQuantity = 10;
+    expect(buttonQuantity * 2);
+
+    for (var i = 0; i < buttonQuantity; i++)
+    {
+        var btn = document.getElementById('btn' + i);
+        QUnit.triggerEvent(btn, "click");
+        var result = txtInput.value[txtInput.value.length - 1];
+        var expected = String(i);
+        equal(result, expected, 'Exepected value: ' + expected + ' Actual value: ' + result);
+
+        var expectedlength = i < 2 ? 1: i;
+        equal(txtInput.value.length, expectedlength +
+            ' Actual value: ' + txtInput.value.length);
+    }
+})
